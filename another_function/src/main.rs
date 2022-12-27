85e3acb7f90d1b7ef2bdaf9a0c5ce37a829eb631
liukai <forfreeday@163.com>
2022-12-27 15:37:39 +0800

    20220916

---
+++ another_function/src/main.rs
@@ -0,0 +1,14 @@
+
+fn main() {
+    println!("Hello, world!");
+    print_labeled_measurement(5, 'h');
+//    another_function(5);
+}
+
+fn another_function(x: i32) {
+  println!("The value of x is: {}", x);
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
---
+++ variables/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let x = 5;
+    println!("test-->{}", x);
+    let x = 6 + 1;
+    println!("test-->{}", x);
+}
