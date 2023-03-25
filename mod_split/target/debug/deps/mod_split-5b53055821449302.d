2de40f5c39156a34c1e604c573639f32be41be6e
liukai <kayle.liu@tron.network>
2023-03-25 20:38:20 +0800

    0325

--- mod_split/target/debug/deps/mod_split-5b53055821449302.d
+++
@@ -1,5 +0,0 @@
-/Users/liukai/workspaces/rust/rust-learning/mod_split/target/debug/deps/mod_split-5b53055821449302.rmeta: src/main.rs
-
-/Users/liukai/workspaces/rust/rust-learning/mod_split/target/debug/deps/mod_split-5b53055821449302.d: src/main.rs
-
-src/main.rs:
